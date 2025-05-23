8fedfd4def43d7a2581a26c426f93003f3912fb9
Kiara Maldonado <maldonadogarciakiara@gmail.com>
2025-05-23 16:10:01 +0100

    Problem 206: Reverse Linked List

---
+++ EasyProblems/Js/206ReverseLinkedList.js
@@ -0,0 +1,26 @@
+// Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+  let prev = null;
+  let current = head;
+
+  while (current !== null) {
+    let nextNode = current.next;
+    current.next = prev;
+    prev = current;
+    current = nextNode;
+  }
+
+  return prev;
+};

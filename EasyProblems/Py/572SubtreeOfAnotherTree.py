a5b82be430fcca2ead03ad4b080b17531d5d45ca
Kiara Maldonado <maldonadogarciakiara@gmail.com>
2025-05-23 17:54:10 +0100

    Problem 572: Subtree of Another Tree

---
+++ EasyProblems/Py/572SubtreeOfAnotherTree.py
@@ -0,0 +1,35 @@
+# Given the roots of two binary trees root and subRoot, return true if there is a subtree of root with the same structure and node values of subRoot and false otherwise.
+
+# A subtree of a binary tree tree is a tree that consists of a node in tree and all of this node's descendants. The tree tree could also be considered as a subtree of itself.
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isSubtree(self, root, subRoot):
+        """
+        :type root: Optional[TreeNode]
+        :type subRoot: Optional[TreeNode]
+        :rtype: bool
+        """
+        if not root:
+            return False  
+        
+        if self.isSameTree(root, subRoot):
+            return True
+        
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+
+    def isSameTree(self, s, t):
+        if not s and not t:
+            return True
+        if not s or not t:
+            return False
+        if s.val != t.val:
+            return False
+        
+        return self.isSameTree(s.left, t.left) and self.isSameTree(s.right, t.right)
+        
